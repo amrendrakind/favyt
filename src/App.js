41c630675309bdab47dab3b5cbbd6f37cc4c6218
amrendrakind <vision2010@gmail.com>
2021-10-09 19:22:05 +0530

    Favyt Deployment

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This page belongs to Favyt: Your Favorite Collections!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.favyt.in"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Favyt
         </a>
       </header>
     </div>
