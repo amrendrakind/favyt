331154917d84bde550d7f41fea71e92201f4834c
amrendrakind <vision2010@gmail.com>
2021-10-30 23:56:23 +0530

    index added

--- src/frontend/template/Product.js
+++ src/frontend/template/Product.js
@@ -2,14 +2,38 @@ import React from "react";
 import './Main.css'
 const Product = ()=>{
     return (
-        <div >
+        <body>
+    <div class="grid-container">
+        <header>
+            <div>
+                <a href="index.html">Favyt</a>
+            </div>
+            <div>
+                <a href="cart.html">Cart</a>
+                <a href="signin">Sign In</a>
+            </div>  
+        </header>
+        <main>
             <ul>
                 <li>Product 1</li>
                 <li>Product 2</li>
                 <li>Product 3</li>
                 <li>Product 4</li>
             </ul>
-        </div>
+        </main>
+        <footer>All right reserved</footer>
+    </div>
+    
+</body>
+
+        // <div >
+        //     <ul>
+        //         <li>Product 1</li>
+        //         <li>Product 2</li>
+        //         <li>Product 3</li>
+        //         <li>Product 4</li>
+        //     </ul>
+        // </div>
     )
 }
 export default Product
