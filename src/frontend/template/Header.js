530f304837bfe037d135a155eded8b4fae4026db
amrendrakind <vision2010@gmail.com>
2021-11-01 23:59:55 +0530

    Favyt Update

--- src/frontend/template/Header.js
+++ src/frontend/template/Header.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './Header.css'
- const Header =()=>{
+const Header = () => {
     return (
-        <div className="header">
-            <div>
-                <a href = "index.html">Favyt</a>
+        <header>
+            <div className="">
+                <div>
+                    <a href="index.html">Favyt</a>
+                </div>
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
             </div>
-            <div>
-            <a href = "cart.html">Cart</a>
-            <a href = "signin.html">Sign In</a>
-            </div>
-        </div>
+        </header>
     )
- }
- export default Header
+}
+export default Header
